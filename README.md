fc1888d58d42c65f908824e16eb39047e772ed32
4undRaiser <jonathaniheme@gmail.com>
2023-02-11 06:33:01 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# celo-devoting
- 
+# Celogram
+
+A full stack social media dapp built on the celo blockchain
+
+- Clone the repo to your computer.
+- open the project from from vscode.
+- Run `npm install` command to install all the dependencies required to run the app locally.
+
+## Demo
+
+[link](https://fanciful-tartufo-92a809.netlify.app/)
--- src/contracts/celogram.sol
+++ src/contracts/celogram.sol
@@ -2,10 +2,22 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+interface IERC20Token {
+  function transfer(address, uint256) external returns (bool);
+  function approve(address, uint256) external returns (bool);
+  function transferFrom(address, address, uint256) external returns (bool);
+  function totalSupply() external view returns (uint256);
+  function balanceOf(address) external view returns (uint256);
+  function allowance(address, address) external view returns (uint256);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
 
 contract Celogram{
 
 uint private postLength = 0;
+address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
 
     struct Post{
         address user;
@@ -72,8 +84,19 @@ uint private postLength = 0;
          );
     }
 
+    function sendTip(uint _index, uint _ammount) public payable  {
+        require(
+          IERC20Token(cUsdTokenAddress).transferFrom(
+             msg.sender,
+             posts[_index].user,
+             _ammount
+          ),
+          "Transfer failed."
+        );
+    }
 
     function getPostsLength() public view returns(uint){
         return(postLength);
-    }
+    }     
+
 }
